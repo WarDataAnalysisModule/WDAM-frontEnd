04ef66151f326594487e212e8c584dd1ef1efa75
ohyeong <oueong@naver.com>
2024-05-24 16:37:37 +0900

    Feat: api 6번 로그인 토큰 재발급 추가 & api 3,4,5 수정

--- src/component/page/MyPage.jsx
+++ src/component/page/MyPage.jsx
@@ -24,16 +24,20 @@ function MyPage(props) {
     const [phoneNumber, setPhoneNumber] = useState(''); // 전화번호
     const passwordCheck = password === verifyPwd; // 비밀번호 확인 시 체크하는 변수 (boolean)
     const [pwVb, setPwVb] = useState(false); // 비밀번호 암호화
-    const headerData = JSON.parse(localStorage.getItem('headerData')); // 전역변수로 저장된 accessToken 불러옴
 
     useEffect(() => {
-        if (!localStorage.getItem('headerData')) navigate('/');
-        check();
-    }, [navigate])
+        if (!localStorage.getItem('headerData')) {
+            navigate('/');
+        } else {
+            check();
+        } 
+    }, [])
 
 
     const check = async() => {
         try {
+            const headerData = JSON.parse(localStorage.getItem('headerData'));
+
             const response = await fetch('http://localhost:8080/users', { // 마이페이지 조회
                 method: 'GET',
                 headers: {
@@ -41,15 +45,27 @@ function MyPage(props) {
                     'Authorization': headerData // accessToken
                 }
             });
-            const responseData = await response.json(); // json으로 response 받기
-            // 서버 응답에 따른 처리
-            if (response.ok && responseData.code === "1000") { // 정상적인 연결
-                setName(responseData.data.userName);
-                setPhoneNumber(responseData.data.phone);
-                setEmail(responseData.data.email); // 이름, 전화번호, 이메일 저장
+
+            if (response.ok) {
+                const responseData = await response.json(); // json으로 response 받기
+                console.log(responseData);
+    
+                if (responseData.code === "1000") { // 정상적인 연결
+                    setName(responseData.data.userName);
+                    setPhoneNumber(responseData.data.phone);
+                    setEmail(responseData.data.email); // 이름, 전화번호, 이메일 저장
+                } else {
+                    // 데이터 가져오기 실패
+                    alert("GET 실패");
+                }
+            } else if (response.status === 401) {
+                const retryResult = await retry();
+                if (retryResult) {
+                    check();         // 재시도
+                }
             } else {
-                // 데이터 가져오기 실패
-                alert("GET 실패");
+                // 다른 HTTP status인 경우
+                alert(`GET 실패: ${response.status}`);
             }
         }
         catch (error) {
@@ -65,6 +81,7 @@ function MyPage(props) {
                 return;
             }
 
+            const headerData = JSON.parse(localStorage.getItem('headerData'));
             let updatedInfo = {};
             if (password) updatedInfo.password = password; // 비밀번호 수정될 시 
             else updatedInfo.password = null; // 안될 시
@@ -79,18 +96,29 @@ function MyPage(props) {
                 },
                 body: JSON.stringify(updatedInfo)
             });
-            
-            const responseData = await response.json();
 
             // 서버 응답에 따른 처리
-            if (response.ok && responseData.code === "1000") {
-                // 정보 수정 성공
-                setPassword('');
-                setVerifyPwd('');
-                alert("정보 수정 성공");
+            if (response.ok) {
+                const responseData = await response.json();
+                console.log(responseData);
+
+                if(responseData.code === "1000"){
+                    // 정보 수정 성공
+                    setPassword('');
+                    setVerifyPwd('');
+                    alert("정보 수정 성공");
+                } else {
+                    // 정보 수정 실패
+                    alert("정보 수정 실패");
+                }
+            } else if(response.status === 401){
+                const retryResult = await retry();
+                if (retryResult) {
+                    handleSubmit();         // 재시도
+                }
             } else {
                 // 정보 수정 실패
-                alert("정보 수정 실패");
+                alert(`정보 수정 실패: ${response.status}`);
             }
         }
         catch (error) {
@@ -117,23 +145,95 @@ function MyPage(props) {
                 })
             });
             
-            const responseData = await response.json();
-            
             // 서버 응답에 따른 처리
-            if (response.ok && responseData.code === "1000") {
-                localStorage.setItem('headerData', '');
-                localStorage.setItem('accessToken', '');
-                localStorage.setItem('refreshToken', '');
-                alert("로그아웃 되었습니다.");
-                navigate('/');
+            if (response.ok) {
+                const responseData = await response.json();
+                console.log(responseData);
+
+                if(responseData.code === "1000"){
+                    localStorage.setItem('headerData', '');
+                    localStorage.setItem('accessToken', '');
+                    localStorage.setItem('refreshToken', '');
+                    alert("로그아웃 되었습니다.");
+                    navigate('/');
+                } else {
+                    // 로그아웃 실패
+                    alert("로그아웃 실패");
+                }
+            } else if(response.status === 401){
+                const retryResult = await retry();
+                if (retryResult) {
+                    logout();         // 재시도
+                }
             } else {
-                // 로그아웃 실패
-                alert("로그아웃 실패");
+                // 다른 HTTP status인 경우
+                alert(`로그아웃 실패: ${response.status}`);
             }
         }
         catch (error) {
             console.error('서버 에러:', error);
             alert("로그아웃 실패");
+        }
+    }
+
+    const retry = async() => {
+        const accessToken = JSON.parse(localStorage.getItem('accessToken'));
+        const refreshToken = JSON.parse(localStorage.getItem('refreshToken'));
+
+        try {
+            const response = await fetch('http://localhost:8080/users/reissue', { // 마이페이지 조회
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    accessToken: accessToken,
+                    refreshToken: refreshToken
+                })
+            });
+        
+            // 서버 응답에 따른 처리
+            if (response.ok) {
+                const responseData = await response.json();
+
+                if(responseData.code === "1000"){
+                    // 바디와 Authorization 저장                
+                    const headerData = response.headers.get('Authorization');
+                    const accessToken = responseData.data.accessToken;
+                    const refreshToken = responseData.data.refreshToken;
+                    
+                    console.log(headerData);
+                    // 로컬 스토리지 (전역 변수)에 저장
+                    localStorage.setItem('headerData', JSON.stringify(headerData));
+                    localStorage.setItem('accessToken', JSON.stringify(accessToken));
+                    localStorage.setItem('refreshToken', JSON.stringify(refreshToken));
+
+                    console.log("토큰발급 완료: ", responseData);
+                    return true;
+                } else {
+                    // 토근재발급 실패
+                    alert("로그아웃됨");
+                    return false;
+                }
+                
+            } else if(response.status === 401){
+                // 토근재발급 실패
+                localStorage.setItem('headerData', '');
+                localStorage.setItem('accessToken', '');
+                localStorage.setItem('refreshToken', '');
+                alert("로그아웃 되었습니다.");
+                navigate('/');
+                return false;
+            } else {
+                // 다른 HTTP status인 경우
+                alert(`토큰발급 실패: ${response.status}`);
+                return false;
+            }
+        }
+        catch (error) {
+            console.error('토큰발급:', error);
+            alert("로그아웃됨");
+            return false;
         }
     }
 
@@ -143,7 +243,7 @@ function MyPage(props) {
                 <Button type="tag" title="로그아웃" onClick={logout}/>
             </ButtonContainer>
         <Wrapper>
-            <Link to="/">
+            <Link to="/FileUpload">
                 <Container2>
                     <img src={icon} alt="img" style={{maxWidth: '300px', height: 'auto', pointerEvents: 'all'}}/>
                 </Container2>
